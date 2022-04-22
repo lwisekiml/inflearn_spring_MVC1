112a1c4bbb27cee6f64afa91a13f4dc2b8dc69dd
wisekim <lwisekiml@naver.com>
2022-04-22 23:55:17 +0900

    [스프링 MVC - 웹 페이지 만들기] 상품 상세

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -33,6 +34,16 @@ public class BasicItemController {
         return "basic/items";
     }
 
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
     @PostConstruct // 해당 빈의 의존관계가 모두 주입되고 나면 초기화 용도로 호출
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
