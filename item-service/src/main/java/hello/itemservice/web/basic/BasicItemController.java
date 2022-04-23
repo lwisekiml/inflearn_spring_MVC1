a1c176b6a62f5f10acc9894d14e2edd2d6101fa0
wisekim <lwisekiml@naver.com>
2022-04-23 20:32:41 +0900

    [스프링 MVC - 웹 페이지 만들기] 상품 등록 처리 - @ModelAttribute

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -5,10 +5,7 @@ import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -49,9 +46,61 @@ public class BasicItemController {
     }
 
     // 상품 등록 처리
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                            @RequestParam int price,
+                            @RequestParam Integer quantity,
+                            Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item); // item.html에서 {item.id} 중 item을 의미
+
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute("item") Item item
+     * model.addAttribute("item", item); 자동 추가
+     *
+     * @ModelAttribute
+     * - 요청 파라미터 처리 : Item 객체를 생성하고, 요청 파라미터의 값을 프로퍼티 접근법(setXxx)으로 입력해준다.
+     * - Model 추가 : Model에 @ModelAttribute로 지정한 객체를 자동으로 넣어준다.
+     * (데이터를 담을 때 이름은 @ModelAttribute에 지정한 name(value / 여기서는 ("item")값) 속성을 사용한다.
+     *
+     * @ModelAttribute("hello") Item item --> 이름을 hello 로 지정
+     * model.addAttribute("hello", item); --> 모델에 hello 이름으로 저장
+     */
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item/*, Model model*/) {
+        itemRepository.save(item);
+//        model.addAttribute("item", item); // 자동 추가, 생략 가능
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute name 생략 가능
+     * model.addAttribute(item); 자동 추가, 생략 가능
+     * 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
+     */
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute 자체 생략 가능
+     * model.addAttribute(item) 자동 추가
+     */
     @PostMapping("/add")
-    public String save() {
-        return "xxx";
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+        return "basic/item";
     }
 
     /**
